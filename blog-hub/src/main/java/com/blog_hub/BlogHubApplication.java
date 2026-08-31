cf0a64ccd875bb718ca76980195323bf007ed14a
sabbir-sharif <sabbirshariff1996@gmail.com>
2026-09-01 00:02:47 +0600

    configured redis

--- blog-hub/src/main/java/com/blog_hub/BlogHubApplication.java
+++ blog-hub/src/main/java/com/blog_hub/BlogHubApplication.java
@@ -2,9 +2,11 @@ package com.blog_hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCaching
 public class BlogHubApplication {
 
 	public static void main(String[] args) {
